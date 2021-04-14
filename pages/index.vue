a42f622152c17eeef1d156b2a6059febaa265d71
programando <jhonjamesmg@hotmail.com>
2021-04-13 20:55:06 -0500

    revisión

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="font-rale">
-    <div class="mt-2 flex -ml-4 justify-center">
+    <div class="flex justify-center mt-2 -ml-4">
       <a href="http://www.balquimia.com">
         <img class="h-20" src="/logo.png" alt="Balquimia.com" />
       </a>
@@ -8,7 +8,7 @@
     <div class="flex justify-center mt-14">
       <h2 class="text-4xl text-primary">Pagos en Linea</h2>
     </div>
-    <div class="mt-8 mx-10 border py-2 border-primary rounded-lg ">
+    <div class="py-2 mx-10 mt-8 border rounded-lg border-primary ">
       <div class="flex justify-center">
         <h2 class="text-xl">Tipo de pago a realizar</h2>
       </div>
@@ -16,28 +16,29 @@
         <div class="flex flex-col items-center justify-center mt-2">
           <form class="flex flex-col">
             <div>
+             
+              <nuxt-link to="pin">
               <input
                 type="radio"
                 id="contactChoice1"
                 name="contact"
                 value="phone"
               />
+          
               <label class="px-2 text-lg" for="contactChoice1"
                 >Numero Pin</label
               >
-            </div>
-            <div class="mt-2">
-              <input
-                type="radio"
-                id="contactChoice2"
-                name="contact"
-                value="phone"
-              />
-              <label class="px-2 text-lg" for="contactChoice2"
-                >Numero Pedido</label
-              >
+                </nuxt-link>
+
             </div>
 
+            <nuxt-link to="pedido">
+                  <div class="mt-2">
+                    <input    type="radio"   id="contactChoice2"     name="contact"   value="phone"  />
+                    <label class="px-2 text-lg" for="contactChoice2">Número Pedido</label>
+                  </div>
+             </nuxt-link>
+          
             <div class="mt-2">
               <input
                 type="radio"
@@ -60,18 +61,18 @@
                 >Libre Registro</label
               >
             </div>
-            <div class="mt-4 flex justify-center space-x-3">
+            <div class="flex justify-center mt-4 space-x-3">
               <div>
                 <nuxt-link
                   to="/"
-                  class="w-28 px-2 py-1 border rounded-lg bg-primary text-white border-primary flex justify-center"
+                  class="flex justify-center px-2 py-1 text-white border rounded-lg w-28 bg-primary border-primary"
                   
                 >
                   Cancelar</nuxt-link
                 >
               </div>
               <button
-                class="w-28 px-4 py-1 border rounded-md bg-primary text-white"
+                class="px-4 py-1 text-white border rounded-md w-28 bg-primary"
                 type="submit"
               >
                 Seleccionar
@@ -81,30 +82,9 @@
         </div>
       </div>
     </div>
-    <div class="mx-10 mt-2 ">
-      <nuxt-link
-        class="px-2 border py-1 rounded-lg border-primary"
-        to="numeroPin"
-        >Numero Pin</nuxt-link
-      >
-      <nuxt-link
-        class="px-2 border py-1 rounded-lg border-primary"
-        to="numeroPedido"
-        >Numero Pedido</nuxt-link
-      >
-    </div>
-    <div class="mx-10 mt-2">
-      <nuxt-link
-        class="px-2 border py-1 rounded-lg border-primary"
-        to="numeroFactura"
-        >Numero Factura</nuxt-link
-      >
-      <nuxt-link
-        class="px-2 border py-1 rounded-lg border-primary"
-        to="libreRegistro"
-        >Libre Registro</nuxt-link
-      >
-    </div>
+
+ 
+ 
   </div>
 </template>
 
--- pages/libre.vue
+++ pages/libre.vue
@@ -1,48 +1,48 @@
 <template>
   <div class="mx-10 mt-4">
-    <div class="mt-2 flex -ml-4 justify-center">
+    <div class="flex justify-center mt-2 -ml-4">
       <a href="http://www.balquimia.com">
         <img class="h-20" src="/logo.png" alt="Balquimia.com" />
       </a>
     </div>
-    <div class="flex justify-center text-lg sm:text-xl text-primary border-t border-l border-r border-primary mt-10">
+    <div class="flex justify-center mt-10 text-lg border-t border-l border-r sm:text-xl text-primary border-primary">
       <h1>Pago Libre Registro</h1>
     </div>
-    <div class="border border-primary px-4 py-4">
-      <div class="grid sm:grid-cols-2 mx-4">
-        <label class="flex justify-center">Identificacion</label>
+    <div class="px-4 py-4 border border-primary">
+      <div class="grid mx-4 sm:grid-cols-2">
+        <label class="flex justify-center">Identificación</label>
         <div class="flex justify-center">
-          <input class="border rounded-lg px-2 py-1 focus:outline-none w-48 sm:w-56" type="text">
+          <input class="w-48 px-2 py-1 border rounded-lg focus:outline-none sm:w-56" type="text">
         </div>
       </div>
-      <div class="grid sm:grid-cols-2 mx-4">
+      <div class="grid mx-4 sm:grid-cols-2">
         <label class="flex justify-center">Email</label>
         <div class="flex justify-center">
-          <input class="border rounded-lg px-2 py-1 focus:outline-none w-48 sm:w-56" type="text">
+          <input class="w-48 px-2 py-1 border rounded-lg focus:outline-none sm:w-56" type="text">
         </div>
       </div>
-      <div class="grid sm:grid-cols-2 mx-4">
+      <div class="grid mx-4 sm:grid-cols-2">
         <label class="flex justify-center">Cliente</label>
         <div class="flex justify-center">
-          <input class="border rounded-lg px-2 py-1 focus:outline-none w-48 sm:w-56" type="text">
+          <input class="w-48 px-2 py-1 border rounded-lg focus:outline-none sm:w-56" type="text">
         </div>
       </div>
-      <div class="grid sm:grid-cols-2 mx-4">
+      <div class="grid mx-4 sm:grid-cols-2">
         <label class="flex justify-center">Vr. Pagar</label>
         <div class="flex justify-center">
-          <input class="border rounded-lg px-2 py-1 focus:outline-none w-48 sm:w-56" type="text">
+          <input class="w-48 px-2 py-1 border rounded-lg focus:outline-none sm:w-56" type="text">
         </div>
       </div>
       
 
-      <div class="mt-4 flex justify-center">
+      <div class="flex justify-center mt-4">
         <ButtonCancel
           text="Cancelar"
           to="/"
           width="w-28"
         ></ButtonCancel>
         <div class="ml-2">
-          <button class="w-28 px-2 py-1 border rounded-lg bg-primary text-white border-primary">Pagar</button>
+          <button class="px-2 py-1 text-white border rounded-lg w-28 bg-primary border-primary">Pagar</button>
         </div>
       </div>  
       
