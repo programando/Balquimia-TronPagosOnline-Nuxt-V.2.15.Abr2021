6ce67eb8bd66f9024d03106876a98dfbbc130f74
Luis Jose <luisjose.morales2@gmail.com>
2021-04-14 18:52:43 -0400

    modificando los inpunts

--- pages/factura.vue
+++ pages/factura.vue
@@ -1,45 +1,58 @@
 <template>
   <div class="mx-10 mt-4">
-    <div class="mt-2 flex -ml-4 justify-center">
+    <div
+      class="flex justify-center mt-2 -ml-4 md:justify-around md:ml-4 md:mt-8"
+    >
       <a href="http://www.balquimia.com">
-        <img class="h-20 md:h-auto" src="/logo.png" alt="Balquimia.com" />
+        <img
+          class="h-24 mt-4"
+          src="/logo.png"
+          alt="Balquimia.com"
+        />
       </a>
+      <div class="hidden lg:block">
+        <img class="h-40" src="PayU.png" alt="pagos    " />
+      </div>
     </div>
 
-    <div class="flex justify-center items-center mt-8 md:mt-14">
-      <div class="border border-primary px-10 py-4">
+    <div class="flex items-center justify-center mt-8 md:mt-14">
+      <div class="px-10 py-4 border border-primary">
         <div
-          class="text-lg sm:text-xl lg:text-2xl text-primary pb-4   "
+          class="pb-4 text-lg sm:text-xl lg:text-2xl text-primary "
         >
           <h1 class="text-center">Pago con su numero de Factura</h1>
         </div>
-        <div class="grid sm:grid-cols-2 mx-4">
+        <div class="grid mx-4 sm:grid-cols-2">
           <label class="flex justify-center md:justify-start md:ml-4">Numero de Factura</label>
           <div class="flex justify-center">
             <input
-              class="border rounded-lg px-2 py-1 focus:outline-none w-48 sm:w-56"
+              class="w-12 px-2 py-1 border rounded-lg focus:outline-none sm:w-16 pocision"
+              type="text"
+            />
+            <input
+              class="px-2 py-1 border rounded-lg w-36 focus:outline-none sm:w-40 pocision"
               type="text"
             />
           </div>
         </div>
-        <div>
+        <div class="mt-2">
           <InputDisable text="Identificacion"></InputDisable>
         </div>
-        <div>
+        <div class="mt-2">
           <InputDisable text="Cliente"></InputDisable>
         </div>
-        <div>
+        <div class="mt-2">
           <InputDisable text="Email"></InputDisable>
         </div>
-        <div>
+        <div class="mt-2">
           <InputDisable text="Vr. Pagar"></InputDisable>
         </div>
 
-        <div class="mt-4 flex justify-center">
+        <div class="flex justify-center mt-4">
           <ButtonCancel text="Cancelar" to="/" width="w-28"></ButtonCancel>
           <div class="ml-2">
             <button
-              class="w-28 px-2 py-1 border rounded-lg bg-primary text-white border-primary"
+              class="px-2 py-1 text-white border rounded-lg w-28 bg-primary border-primary"
             >
               Pagar
             </button>
@@ -60,3 +73,9 @@ export default {
   }
 };
 </script>
+
+<style>
+.pocision {
+  direction: rtl;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="font-rale">
-    <div class="flex justify-center md:justify-around mt-2 -ml-4 md:ml-4 md:mt-8">
+    <div
+      class="flex justify-center mt-2 -ml-4 md:justify-around md:ml-4 md:mt-8"
+    >
       <a href="http://www.balquimia.com">
-        <img class="h-20 md:h-auto lg:h-28 mt-4" src="/logo.png" alt="Balquimia.com" />
+        <img
+          class="h-24 mt-4"
+          src="/logo.png"
+          alt="Balquimia.com"
+        />
       </a>
       <div class="hidden lg:block">
-        <img class="h-40" src="PayU.png" alt="pagos    ">
+        <img class="h-40" src="PayU.png" alt="pagos    " />
       </div>
     </div>
     <div class="flex justify-center mt-14">
       <h2 class="text-4xl lg:text-5xl text-primary">Pagos en Linea</h2>
     </div>
-    <div class="flex justify-center items-center">
-      <div class="mt-8 px-10 md:px-20 py-6 border  border-primary ">
+    <div class="flex items-center justify-center">
+      <div class="px-10 py-6 mt-8 border md:px-20 border-primary ">
         <div class="flex justify-center">
           <h2 class="text-xl">Tipo de pago a realizar</h2>
         </div>
@@ -21,52 +27,28 @@
             <form class="flex flex-col">
               <div>
                 <nuxt-link to="pin">
-                  <input
-                    type="radio"
-                    id="contactChoice1"
-                    name="contact"
-                    value="phone"
-                  />
-                  <label class="px-2 text-lg" for="contactChoice1"
+                  <label class="px-2 text-lg cursor-pointer hover:text-primary" for="contactChoice1"
                     >Numero Pin</label
                   >
                 </nuxt-link>
               </div>
               <div class="mt-2">
                 <nuxt-link to="pedido">
-                  <input
-                    type="radio"
-                    id="contactChoice2"
-                    name="contact"
-                    value="phone"
-                  />
-                  <label class="px-2 text-lg" for="contactChoice2"
+                  <label class="px-2 text-lg cursor-pointer hover:text-primary" for="contactChoice2"
                     >Número Pedido</label
                   >
                 </nuxt-link>
               </div>
               <div class="mt-2">
                 <nuxt-link to="factura">
-                  <input
-                    type="radio"
-                    id="contactChoice3"
-                    name="contact"
-                    value="mail"
-                  />
-                  <label class="px-2 text-lg" for="contactChoice3"
+                  <label class="px-2 text-lg cursor-pointer hover:text-primary" for="contactChoice3"
                     >Numero Factura</label
                   >
                 </nuxt-link>
               </div>
               <div class="mt-2">
                 <nuxt-link to="libre">
-                  <input
-                    type="radio"
-                    id="contactChoice4"
-                    name="contact"
-                    value="mail"
-                  />
-                  <label class="px-2 text-lg" for="contactChoice4"
+                  <label class="px-2 text-lg cursor-pointer hover:text-primary" for="contactChoice4"
                     >Libre Registro</label
                   >
                 </nuxt-link>
