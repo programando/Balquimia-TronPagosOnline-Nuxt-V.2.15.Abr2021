e9b711ca757a8e3ce377300331869eeb39dc1f97
programando <jhonjamesmg@hotmail.com>
2021-04-15 21:30:29 -0500

    v-model to component

--- pages/index.vue
+++ pages/index.vue
@@ -4,36 +4,39 @@
       class="flex justify-center mt-2 -ml-4 md:justify-around md:ml-4 md:mt-8"
     >
       <a href="http://www.balquimia.com">
-        <img class="h-24 mt-4" src="/logo.png" alt="Balquimia.com" />
+        <img class="mt-4 h-28" src="/logo.png" alt="Balquimia.com" />
       </a>
       <div class="hidden lg:block">
-        <img class="h-40" src="PayU.png" alt="pagos    " />
+        <img class="h-28" src="PayU.png" alt="pagos    " />
       </div>
     </div>
-    <div class="flex justify-center mt-14">
-      <h2 class="text-4xl lg:text-5xl text-primary">Pagos en Linea</h2>
+    
+    <div class="flex justify-center mt-8">
+      <h2 class="text-4xl lg:text-5xl text-primary">Pagos en Línea</h2>
     </div>
-    <div class="flex items-center justify-center">
-      <div class="py-6 mt-8 border border-primary">
         <div class="flex justify-center">
-          <h2 class="text-lg ld:text-xl">Realize su pago por medio de:</h2>
-        </div>
-        <div class="flex flex-col items-center justify-center mt-2">
+        <h2 class="mt-4 text-2xl lg:text-2xl text-primary">Realize su pago por medio de:</h2>
+      </div>
+
+    <div class="flex items-center justify-center">
+      <div class="py-3 mt-4 border border-primary">
+
+        <div class="flex flex-col items-center justify-center ">
           <form class="w-full mx-20 lg:mx-28">
             <nuxt-link to="pin">
               <div
-                class="w-full text-center hover:bg-gray-200 hover:text-primary"
+                class="w-full p-1 text-center hover:bg-gray-200 hover:text-primary"
               >
                 <label
                   class="px-2 cursor-pointer lg:text-lg"
                   for="contactChoice1"
-                  >Numero Pin</label
+                  >Número Pin</label
                 >
               </div>
             </nuxt-link>
             <nuxt-link to="pedido">
               <div
-                class="w-full mt-2 text-center hover:bg-gray-200 hover:text-primary"
+                class="w-full p-1 mt-2 text-center hover:bg-gray-200 hover:text-primary"
               >
                 <label
                   class="px-2 cursor-pointer lg:text-lg"
@@ -44,18 +47,18 @@
             </nuxt-link>
             <nuxt-link to="factura">
               <div
-                class="w-full mt-2 text-center hover:bg-gray-200 hover:text-primary"
+                class="w-full p-1 mt-2 text-center hover:bg-gray-200 hover:text-primary"
               >
                 <label
                   class="px-2 cursor-pointer lg:text-lg"
                   for="contactChoice3"
-                  >Numero Factura</label
+                  >Número Factura</label
                 >
               </div>
             </nuxt-link>
-            <nuxt-link to="libre">
+            <nuxt-link to="libre" >
               <div
-                class="w-full mt-2 text-center hover:bg-gray-200 hover:text-primary"
+                class="w-full p-1 mt-2 text-center hover:bg-gray-200 hover:text-primary"
               >
                 <label
                   class="px-2 cursor-pointer lg:text-lg"
@@ -64,7 +67,7 @@
                 >
               </div>
             </nuxt-link>
-            <div class="flex justify-center mt-4 space-x-3">
+            <div class="flex justify-center mt-4 mb-4 space-x-3">
               <div>
                 <nuxt-link
                   to="/"
--- pages/pin.vue
+++ pages/pin.vue
@@ -1,50 +1,50 @@
 <template>
   <div class="mx-10 mt-4">
-    <div
-      class="flex justify-center mt-2 -ml-4 md:justify-around md:ml-4 md:mt-8"
-    >
+
+    <div class="flex justify-center mt-2 -ml-4 md:justify-around md:ml-4 md:mt-8"  >
       <a href="http://www.balquimia.com">
-        <img class="h-24 mt-4 " src="/logo.png" alt="Balquimia.com" />
+        <img class="mt-4 h-28 " src="/logo.png" alt="Balquimia.com" />
       </a>
       <div class="hidden lg:block">
-        <img class="h-40" src="PayU.png" alt="pagos    " />
+        <img class="h-28" src="PayU.png" alt="pagos    " />
       </div>
     </div>
+
     <div class="flex items-center justify-center mt-8 md:mt-14">
       <div class="px-10 py-4 border border-primary">
         <div class="pb-4 text-lg sm:text-xl lg:text-2xl text-primary ">
-          <h1 class="text-center">Pago con su numero de Pin</h1>
+          <h1 class="text-center">Pago con número de PIN</h1>
         </div>
         <div class="grid mx-4 sm:grid-cols-2">
           <label class="flex justify-center md:justify-start md:ml-4 lg:text-lg"
-            >Numero de Pin</label
+            >Número de Pin :</label
           >
           <div class="flex justify-center">
             <input
+              @blur="buscarPin"
               class="w-48 px-2 py-1 border rounded-lg focus:outline-none sm:w-56 pocision"
-              type="text"
+              type="text" v-model="pin"
             />
           </div>
         </div>
-        <div class="mt-2">
-          <InputDisable text="Identificacion"></InputDisable>
+         <div class="mt-2">
+          <InputDisable v-model="formUser.nro_identif" text="Identificación :" ></InputDisable>
+        </div>  
+         <div class="mt-2">
+          <InputDisable text="Cliente :" v-model="formUser.nom_full"></InputDisable>
         </div>
         <div class="mt-2">
-          <InputDisable text="Cliente"></InputDisable>
+          <InputDisable text="Email :" v-model="formUser.email"></InputDisable>
         </div>
         <div class="mt-2">
-          <InputDisable text="Email"></InputDisable>
-        </div>
-        <div class="mt-2">
-          <InputDisable text="Vr. Pagar"></InputDisable>
-        </div>
+          <InputDisable text="Vr. a pagar:" v-model="formUser.valor_pagar"></InputDisable>
+        </div> 
 
         <div class="flex justify-center mt-4">
           <ButtonCancel text="Cancelar" to="/" width="w-28"></ButtonCancel>
-          <div class="ml-2">
-            <button
-              class="px-2 py-1 text-white border rounded-lg w-28 bg-primary border-primary"
-            >
+          <div class="ml-2">  
+            
+            <button class="px-2 py-1 text-white border rounded-lg w-28 bg-primary border-primary">
               Pagar
             </button>
           </div>
@@ -57,12 +57,39 @@
 <script>
 import InputDisable from "@/components/inputs/inputDisable";
 import ButtonCancel from "@/components/buttons/buttonCancel";
+import PinesPgoElectronico from "@/models/PinesPgoElectronico";
 export default {
-  components: {
-    InputDisable,
-    ButtonCancel
-  }
-};
+  components: { InputDisable, ButtonCancel },
+  data: ()=>  ({
+        pin :'2104031',
+          formUser : {
+              nro_identif:'94491178',
+              nom_full:'',
+              email:'',
+              valor_pagar:'',
+          },
+  }),
+
+    methods: {
+          buscarPin() {
+               this.pinError = '';
+                
+              PinesPgoElectronico.buscarPin ( this.pin)
+              .then( response => {
+                  if (!response.data || response.data.length == 0) {
+                    this.pinError = 'Número de PIN no encontrado!';
+                     
+                    return ;
+                  }
+                  let Data = response.data[0];
+                  this.formUser.nro_identif = Data.nro_identif;
+                  this.formUser.nom_full    = Data.nom_full;
+                  this.formUser.email       = Data.email;
+                  this.formUser.valor_pin   = Data.valor_pin.toLocaleString() ;
+              })
+          },
+    },
+}
 </script>
 
 <style>
